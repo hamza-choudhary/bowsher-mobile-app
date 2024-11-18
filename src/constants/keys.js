a6a41294b1ca8243ed65dd869c7cefa0ad316590
hamza-choudhary <humzach700@gmail.com>
2024-11-19 00:23:55 +0500

    feature: converter added

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -13,3 +13,12 @@ export const UNITS = {
   gal: {unit: 'gal', name: 'gallons'},
   l: {unit: 'l', name: 'Liter'},
 };
+
+export const nitrogenConversionsDirect = {
+  lb: {kg: 0.4536, scf: 13.803, nm3: 0.3627, gal: 0.1481, l: 0.5606},
+  kg: {lb: 2.2046, scf: 30.42, nm3: 0.7996, gal: 0.3262, l: 1.2349},
+  scf: {lb: 0.0724, kg: 0.0329, nm3: 0.02628, gal: 0.01074, l: 0.04065},
+  nm3: {lb: 2.7571, kg: 1.2506, scf: 38.0518, gal: 0.408, l: 1.5443},
+  gal: {lb: 6.7522, kg: 3.0656, scf: 93.1099, nm3: 2.451, l: 3.785},
+  l: {lb: 1.7838, kg: 0.8098, scf: 24.6002, nm3: 0.6475, gal: 0.2642},
+};
