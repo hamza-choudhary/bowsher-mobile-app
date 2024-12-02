bc87d520acd9031829f7345aafda4e0c94ab05e3
hamza-choudhary <humzach700@gmail.com>
2024-12-02 15:33:22 +0500

    feature(History): no data found component added

---
+++ src/constants/messages.js
@@ -0,0 +1,5 @@
+export const MESSAGES = {
+  NO_DATA_FOUND: 'There are no items in history.',
+  ALERT_DELETE_HISTORY: 'This action will clear all history.',
+  ALERT_DELETE_HISTORY_ITEM: 'This action will delete history item',
+};
---
+++ src/screens/history/components/NoDataFound.jsx
@@ -0,0 +1,54 @@
+import {MESSAGES} from '@constants';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Text, useTheme} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export function NoDataFound({
+  message = '',
+  variant = 'bodyLarge',
+  containerStyle,
+  style,
+  fullScreen = false,
+}) {
+  const {spacing, colors} = useTheme();
+  const messageValue = message || MESSAGES.NO_DATA_FOUND;
+
+  const container = [
+    {paddingHorizontal: spacing.m, paddingVertical: spacing.m},
+    fullScreen && styles.fullScreenContainer,
+    containerStyle,
+  ];
+
+  return (
+    <SafeAreaView style={container}>
+      {fullScreen ? (
+        <>
+          <Icon name="history" size={200} color={colors.primary} />
+          <Text variant="headlineLarge">No Data Found!</Text>
+          <Text variant="titleMedium">{messageValue}</Text>
+        </>
+      ) : (
+        <Text style={style} variant={variant}>
+          {messageValue}
+        </Text>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  fullScreenContainer: {flex: 1, top: '40%', alignItems: 'center'},
+  button: {marginTop: 10},
+});
+
+NoDataFound.propTypes = {
+  message: PropTypes.string,
+  variant: PropTypes.string,
+  containerStyle: PropTypes.object,
+  style: PropTypes.object,
+  fullScreen: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  showOptions: PropTypes.bool,
+};
