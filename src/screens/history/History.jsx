884632a52a0ac5cab6a64ac188de9aab1b7b63e0
hamza-choudhary <humzach700@gmail.com>
2024-12-02 14:00:32 +0500

    feature: alerts added for deletion

--- src/screens/history/History.jsx
+++ src/screens/history/History.jsx
@@ -4,11 +4,14 @@ import {globalStyles as gs} from '@styles';
 import {deleteItemFromLocalStorage, getItemFromLocalStorage} from '@utils';
 import {useCallback, useState} from 'react';
 import {FlatList, View} from 'react-native';
-import {Button, Text, useTheme} from 'react-native-paper';
+import {Button, Dialog, Portal, Text, useTheme} from 'react-native-paper';
 import {ConversionCard} from './components/ConversionCard';
 
 export function History() {
   const [data, setData] = useState([]);
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const {colors, roundness} = useTheme();
+
   const getHistoryFromStorage = useCallback(() => {
     (async () => {
       const history =
@@ -22,19 +25,22 @@ export function History() {
   async function handleDelete() {
     await deleteItemFromLocalStorage(STORAGE.CONVERSION_HISTORY);
     getHistoryFromStorage();
+    setDialogVisible(false)
     //TODO: show toaster
   }
 
   //TODO: create functionality for favorite
 
-  const {colors} = useTheme();
+  const showDialog = () => setDialogVisible(true);
+  const hideDialog = () => setDialogVisible(false);
+
   return (
     <View style={[gs.flex1, {backgroundColor: colors.background}]}>
       <View style={[gs.flexRow, gs.justifyBetween, gs.itemsCenter, gs.px3]}>
         <Text variant="titleLarge" style={[gs.textCenter, gs.my3]}>
           History
         </Text>
-        <Button mode="outlined" icon="history" onPress={handleDelete}>
+        <Button mode="outlined" icon="history" onPress={showDialog}>
           clear
         </Button>
       </View>
@@ -47,6 +53,23 @@ export function History() {
         contentContainerStyle={[gs.pb3, gs.px3]}
         ListEmptyComponent={NoDataFound}
       />
+      <Portal>
+        <Dialog
+          style={{borderRadius: roundness}}
+          visible={dialogVisible}
+          onDismiss={hideDialog}>
+          <Dialog.Title>Are you sure?</Dialog.Title>
+          <Dialog.Content>
+            <Text variant="bodyMedium">
+              this action will clear all history.
+            </Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={handleDelete}>Confirm</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 }
--- src/screens/history/components/ConversionCard.jsx
+++ src/screens/history/components/ConversionCard.jsx
@@ -1,65 +1,96 @@
 import {removeConversionFromStorage} from '@helpers';
 import {globalStyles as gs} from '@styles';
 import PropTypes from 'prop-types';
+import {useState} from 'react';
 import {View} from 'react-native';
-import {Card, IconButton, Text, useTheme} from 'react-native-paper';
+import {
+  Button,
+  Card,
+  Dialog,
+  IconButton,
+  Portal,
+  Text,
+  useTheme,
+} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 
 export function ConversionCard({data, refetch}) {
   const {colors, roundness} = useTheme();
+  const [dialogVisible, setDialogVisible] = useState(false);
 
   const {id, type, sourceUnit, targetValue, targetUnit} = data;
 
   async function handleDelete() {
     await removeConversionFromStorage(id);
     refetch();
+    setDialogVisible(false);
     //TODO: show toaster
   }
 
   // TODO: add remove history button + logic
 
+  const showDialog = () => setDialogVisible(true);
+  const hideDialog = () => setDialogVisible(false);
+
   return (
-    <Card style={[gs.mb3, {borderRadius: roundness}]}>
-      <Card.Content style={gs.gap2}>
-        <View style={[gs.justifyBetween, gs.itemsCenter, gs.flexRow]}>
-          <Text
-            variant="titleMedium"
-            style={[gs.capitalize, {color: colors.primary}]}>
-            {type}
-          </Text>
-          <View style={[gs.flexRow]}>
-            <IconButton
-              icon="star-outline"
-              iconColor={colors.primary}
-              size={25}
-            />
-            <IconButton
-              onPress={handleDelete}
-              icon="trash-can-outline"
-              iconColor={colors.primary}
-              size={25}
-            />
-          </View>
-        </View>
-        <View style={[gs.flexRow, gs.flex1]}>
-          <View style={[gs.itemsCenter, gs.flex1, gs.justifyBetween]}>
-            <Text variant="bodyLarge">{data.sourceValue}</Text>
-            <Text variant="labelLarge" style={gs.uppercase}>
-              {sourceUnit}
+    <>
+      <Card style={[gs.mb3, {borderRadius: roundness}]}>
+        <Card.Content style={gs.gap2}>
+          <View style={[gs.justifyBetween, gs.itemsCenter, gs.flexRow]}>
+            <Text
+              variant="titleMedium"
+              style={[gs.capitalize, {color: colors.primary}]}>
+              {type}
             </Text>
+            <View style={[gs.flexRow]}>
+              <IconButton
+                icon="star-outline"
+                iconColor={colors.primary}
+                size={25}
+              />
+              <IconButton
+                onPress={showDialog}
+                icon="trash-can-outline"
+                iconColor={colors.primary}
+                size={25}
+              />
+            </View>
           </View>
-          <View style={[gs.mx2, gs.itemsCenter, gs.justifyCenter]}>
-            <Icon color={colors.primary} size={30} name="arrow-right-long" />
+          <View style={[gs.flexRow, gs.flex1]}>
+            <View style={[gs.itemsCenter, gs.flex1, gs.justifyBetween]}>
+              <Text variant="bodyLarge">{data.sourceValue}</Text>
+              <Text variant="labelLarge" style={gs.uppercase}>
+                {sourceUnit}
+              </Text>
+            </View>
+            <View style={[gs.mx2, gs.itemsCenter, gs.justifyCenter]}>
+              <Icon color={colors.primary} size={30} name="arrow-right-long" />
+            </View>
+            <View style={[gs.itemsCenter, gs.flex1, gs.justifyBetween]}>
+              <Text variant="bodyLarge">{targetValue}</Text>
+              <Text variant="labelLarge" style={gs.uppercase}>
+                {targetUnit}
+              </Text>
+            </View>
           </View>
-          <View style={[gs.itemsCenter, gs.flex1, gs.justifyBetween]}>
-            <Text variant="bodyLarge">{targetValue}</Text>
-            <Text variant="labelLarge" style={gs.uppercase}>
-              {targetUnit}
-            </Text>
-          </View>
-        </View>
-      </Card.Content>
-    </Card>
+        </Card.Content>
+      </Card>
+      <Portal>
+        <Dialog
+          style={{borderRadius: roundness}}
+          visible={dialogVisible}
+          onDismiss={hideDialog}>
+          <Dialog.Title>Are you sure?</Dialog.Title>
+          <Dialog.Content>
+            <Text variant="bodyMedium">this action will delete this item.</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={handleDelete}>Confirm</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
   );
 }
 
