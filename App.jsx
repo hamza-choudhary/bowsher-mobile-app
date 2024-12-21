bb4b3674f7ae9216da60cae9bd52d2512961b08e
hamza-choudhary <humzach700@gmail.com>
2024-12-21 12:36:53 +0500

    fix(App): bootsplash hide fixed

--- App.jsx
+++ App.jsx
@@ -1,23 +1,20 @@
 import {AppStatusBar} from '@common';
 import {Navigation} from '@navigation';
 import {themeConfig, toastConfig} from '@utils';
-import {useEffect} from 'react';
 import BootSplash from 'react-native-bootsplash';
 import 'react-native-gesture-handler';
 import {PaperProvider} from 'react-native-paper';
 import Toast from 'react-native-toast-message';
 
 export default function App() {
-  useEffect(() => {
-    (async () => {
-      await BootSplash.hide({fade: true});
-    })();
-  }, []);
-
   return (
     <PaperProvider theme={themeConfig}>
       <AppStatusBar />
-      <Navigation />
+      <Navigation
+        onReady={() => {
+          BootSplash.hide();
+        }}
+      />
       <Toast position="top" config={toastConfig} />
     </PaperProvider>
   );
